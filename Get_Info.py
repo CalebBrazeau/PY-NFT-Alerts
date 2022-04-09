beb34b7eeaa70e204b20cddc93d7b71dfc36cd0a
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-04-09 11:01:35 -0400

    Move getLaunchpad function to Get_Info file

--- Get_Info.py
+++ Get_Info.py
@@ -1,4 +1,6 @@
 import Get_Con as conn
+import datetime
+
 
 def getListingInfo(collection_name):
     """Takes in a collection name and returns listing stats for specified collection"""
@@ -10,3 +12,34 @@ def getTokenInformation(token):
     """Takes in a token address and returns information about specified token"""
     # Get and return API response for token information
     return conn.getConnection("/v2/tokens/" + token)
+
+
+def getLaunchpadInfo():
+    """Retrieves and returns all upcoming collections on the launchpad"""
+    # Initialize array to store retreived collections
+    arr = []
+    # Get and return API request for launchpad items
+    data = conn.getConnection("/v2/launchpad/collections?offset=0&limit=200")
+
+    # Loop through all items in the API response
+    for x in data:
+        try:
+            # Extract year, month, and day information from each element
+            y,m,d = x["launchDatetime"].split("-")
+            # Create datetime variable using extracted year, month, and day
+            launch_time = datetime.datetime(int(y),int(m),int(d.split("T")[0]))
+            # Get current time for comparison
+            now = datetime.datetime.now()
+
+            # If the launch time of the collection is greater than the current time
+            if(launch_time > now):
+                # Append the collection to the array
+                arr.append({
+                    'name': x['name'],
+                    'price': x['price'],
+                    'size': x['size'],
+                    'launchTime': launch_time
+                })
+        except:
+            print("No time :/")
+    return arr
