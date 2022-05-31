33348fb59b4a7e9bd36ab6947a4376a834c65b59
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-05-31 13:32:45 -0400

    Remove unused functions, variables, and imports

--- req.py
+++ req.py
@@ -1,16 +1,7 @@
 import Get_Info as info
-import Send_Alert as alert
-import creds
 from console import console
 from rich.table import Table
 from rich import box
-
-# Name of the collection to retreive information from
-collections = ['mintin']
-# Maximum price of NFT to add to array
-max_SOL = 2
-
-send_alerts = True
 
 
 def setup():
@@ -121,52 +112,6 @@ def set_up_alerts():
 # TODO - Make it work
 def generate_collection():
     console.print('[green underline]Generate Collection')
-
-
-def main():
-    for x in collections:
-        # Array containing specified collection return data
-        arr = info.getCollectionListed(x.replace(' ', '_'), max_SOL)
-
-        # Check if return is empty before sending alert
-        if(len(arr) > 0):
-            # Sort array using price as a key
-            arr.sort(key=arrSortKey)
-
-            # Subject for alert
-            subject = '%s listing alerted for %s Collection!' % (len(arr), x)
-            # Body for alert containing the array of retreived NFT's
-            alert_content = '''\nThe following prices were alerted for %s: %s''' % (x, formatArr(arr))
-            # Send the alert
-            alert.sendAlert(subject, alert_content, creds.email) if send_alerts else print('Alerts disabled')
-        else:
-            print('No listings found under %s SOL!' % max_SOL)
-
-
-def formatArr(arr):
-    '''
-    Takes in an array and formats the objects to a more user friendly string
-    Input: [{'price': 2, 'link': 'www.example.com'}, {'price': 2, 'link': 'www.example.com'}]
-    Return: -------------------------------------------------------------
-            price: 2
-            link: www.example.com
-            -------------------------------------------------------------
-            price: 2
-            link: www.example.com
-    '''
-    # Initialize string
-    formated_arr = ''
-    # Loop through each element of the array
-    for x in arr:
-        # Append a seperator to formated_arr for easier readability
-        formated_arr += '\n------------------------------------------------------------------------------------'
-        # Loop through each element's keys
-        for y in x.keys():
-            # Add each key with a prepended new line and keyname to formated_arr
-            formated_arr += '\n%s: %s' % (y,x[y])
-    print(formated_arr) # TEMP
-    # Return the formated array
-    return formated_arr
 
 
 def arrSortKey(e):
